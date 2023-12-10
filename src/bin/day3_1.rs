898e07d058ec83860453a987dfbafe4380e2d31c
Mohamed Faris <mohd.faris07@gmail.com>
2023-12-10 16:57:07 +0300

    day 3_1

--- src/bin/day3_1.rs
+++ src/bin/day3_1.rs
@@ -8,7 +8,11 @@ fn main() -> Result<()> {
     for i in 0..lines.len() {
         let current_line = lines[i];
         let previous_line = if i > 0 { lines[i - 1] } else { "" };
-        let next_line = if i < lines.len() - 1 { lines[i + 1] } else { "" };
+        let next_line = if i < lines.len() - 1 {
+            lines[i + 1]
+        } else {
+            ""
+        };
 
         process_line(previous_line, current_line, next_line, &mut part_numbers);
     }
@@ -19,12 +23,16 @@ fn main() -> Result<()> {
     Ok(())
 }
 
-fn process_line(previous_line: &str, current_line: &str, next_line: &str, part_numbers: &mut Vec<u32>) {
+fn process_line(
+    previous_line: &str,
+    current_line: &str,
+    next_line: &str,
+    part_numbers: &mut Vec<u32>,
+) {
     let mut i = 0;
     while i < current_line.len() {
         let c = current_line.chars().nth(i).unwrap();
         if c.is_digit(10) {
-            // Identify the whole number
             let mut num_str = String::new();
             while i < current_line.len() && current_line.chars().nth(i).unwrap().is_digit(10) {
                 num_str.push(current_line.chars().nth(i).unwrap());
@@ -32,8 +40,13 @@ fn process_line(previous_line: &str, current_line: &str, next_line: &str, part_n
             }
             let num = num_str.parse::<u32>().unwrap();
 
-            // Check for adjacent symbols
-            if is_adjacent_to_symbol(previous_line, current_line, next_line, i - num_str.len(), num_str.len()) {
+            if is_adjacent_to_symbol(
+                previous_line,
+                current_line,
+                next_line,
+                i - num_str.len(),
+                num_str.len(),
+            ) {
                 part_numbers.push(num);
             }
         } else {
@@ -42,11 +55,16 @@ fn process_line(previous_line: &str, current_line: &str, next_line: &str, part_n
     }
 }
 
-fn is_adjacent_to_symbol(previous_line: &str, current_line: &str, next_line: &str, start_index: usize, length: usize) -> bool {
+fn is_adjacent_to_symbol(
+    previous_line: &str,
+    current_line: &str,
+    next_line: &str,
+    start_index: usize,
+    length: usize,
+) -> bool {
     let end_index = start_index + length;
     let line_length = current_line.len();
 
-    // Check adjacent cells in the current line
     if start_index > 0 && current_line.chars().nth(start_index - 1).unwrap() != '.' {
         return true;
     }
@@ -54,7 +72,6 @@ fn is_adjacent_to_symbol(previous_line: &str, current_line: &str, next_line: &st
         return true;
     }
 
-    // Check adjacent cells in the previous line, if it exists
     if !previous_line.is_empty() {
         for i in start_index..=end_index {
             if i < previous_line.len() && previous_line.chars().nth(i).unwrap() != '.' {
@@ -63,7 +80,6 @@ fn is_adjacent_to_symbol(previous_line: &str, current_line: &str, next_line: &st
         }
     }
 
-    // Check adjacent cells in the next line, if it exists
     if !next_line.is_empty() {
         for i in start_index..=end_index {
             if i < next_line.len() && next_line.chars().nth(i).unwrap() != '.' {
