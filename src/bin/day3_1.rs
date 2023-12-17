22eb9ff588f37cb1b4ff55a6496cde600df47783
Mohamed Faris <mohd.faris07@gmail.com>
2023-12-17 18:13:47 +0300

    day3 attempt

--- src/bin/day3_1.rs
+++ src/bin/day3_1.rs
@@ -1,92 +1,57 @@
 use anyhow::Result;
+use std::collections::HashSet;
+
+struct AoC {
+    nums: Vec<PartNumber>,
+    sy
+}
+struct PartNumber {
+    value: i32,
+    points: HashSet<(i32, i32)>,
+}
+
+impl PartNumber {
+    fn new(row: i32, col: i32, ch: char) -> Self {
+        let points = HashSet::from([
+            (row - 1, col - 1), (row, col - 1), (row + 1, col - 1), // left hand side
+            (row - 1, col), (row + 1, col),                          // abobe and below
+            (row - 1, col + 1), (row, col + 1), (row + 1, col + 1),   // right hand side
+        ]);
+        Self {
+            value: (ch as u8 - b'0') as i32,
+            points,
+        }
+    }
+
+    fn add_digit(&mut self, row: i32, col: i32, ch: char) {
+        self.value = self.value * 10 + (ch as u8 - b'0') as i32;
+        self.points
+            .extend([(row - 1, col + 1), (row, col + 1), (row + 1, col + 1)]);
+    }
+}
 
 fn main() -> Result<()> {
     let input = std::fs::read_to_string("src/bin/input3_1.test")?;
     let lines: Vec<&str> = input.lines().collect();
 
-    let mut part_numbers = Vec::new();
-    for i in 0..lines.len() {
-        let current_line = lines[i];
-        let previous_line = if i > 0 { lines[i - 1] } else { "" };
-        let next_line = if i < lines.len() - 1 {
-            lines[i + 1]
-        } else {
-            ""
-        };
-
-        process_line(previous_line, current_line, next_line, &mut part_numbers);
+    let mut cur_number: Option<PartNumber> = None;
+    for (row, line) in lines.iter().enumerate() {
+        for (col, ch) in line.chars().enumerate() {
+            if ch.is_ascii_digit() {
+                if let Some(ref mut num) = cur_number {
+                    num.add_digit(row as i32, col as i32, ch);
+                } else {
+                    cur_number = Some(PartNumber::new(row as i32, col as i32, ch));
+                }
+            } else {
+                if let Some(num) = cur_number.take() {
+                    self.nums.push(num);
+                                    
+                }
+            }
+        }
     }
-
-    let total_sum: u32 = part_numbers.iter().sum();
-    println!("Total sum of part numbers: {}", total_sum);
 
     Ok(())
 }
 
-fn process_line(
-    previous_line: &str,
-    current_line: &str,
-    next_line: &str,
-    part_numbers: &mut Vec<u32>,
-) {
-    let mut i = 0;
-    while i < current_line.len() {
-        let c = current_line.chars().nth(i).unwrap();
-        if c.is_digit(10) {
-            let mut num_str = String::new();
-            while i < current_line.len() && current_line.chars().nth(i).unwrap().is_digit(10) {
-                num_str.push(current_line.chars().nth(i).unwrap());
-                i += 1;
-            }
-            let num = num_str.parse::<u32>().unwrap();
-
-            if is_adjacent_to_symbol(
-                previous_line,
-                current_line,
-                next_line,
-                i - num_str.len(),
-                num_str.len(),
-            ) {
-                part_numbers.push(num);
-            }
-        } else {
-            i += 1;
-        }
-    }
-}
-
-fn is_adjacent_to_symbol(
-    previous_line: &str,
-    current_line: &str,
-    next_line: &str,
-    start_index: usize,
-    length: usize,
-) -> bool {
-    let end_index = start_index + length;
-    let line_length = current_line.len();
-
-    if start_index > 0 && current_line.chars().nth(start_index - 1).unwrap() != '.' {
-        return true;
-    }
-    if end_index < line_length && current_line.chars().nth(end_index).unwrap() != '.' {
-        return true;
-    }
-
-    if !previous_line.is_empty() {
-        for i in start_index..=end_index {
-            if i < previous_line.len() && previous_line.chars().nth(i).unwrap() != '.' {
-                return true;
-            }
-        }
-    }
-
-    if !next_line.is_empty() {
-        for i in start_index..=end_index {
-            if i < next_line.len() && next_line.chars().nth(i).unwrap() != '.' {
-                return true;
-            }
-        }
-    }
-
-    false
-}
